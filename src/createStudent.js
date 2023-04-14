0d295759a5b287dd62e3c5084da12ac8265711a4
Livia Boechat <limaboechatti@gmail.com>
2023-04-14 14:42:42 -0300

    Add files via upload

---
+++ src/createStudent.js
@@ -0,0 +1,34 @@
+/*
+  A função createStudent recebe como parâmetro um nome, e retorna um objeto contendo duas chaves:
+
+    (1) name, contendo o nome passado como parâmetro;
+    (2) feedback, contendo uma função que retorna a frase 'Eita pessoa boa!' ao ser chamada.
+
+  Implemente a função de forma que ela atenda aos testes propostos.
+
+  Parâmetros:
+    - Uma string;
+  Comportamento:
+    const estudante = createStudent('Leandrão, o Lobo Solitário')
+
+    estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
+    estudante.feedback() // Retorna: 'Eita pessoa boa!'
+*/
+
+const estudante = { age: 30 };
+
+const feedback = () => {
+  let msg = 'Eita pessoa boa!';
+  return (msg);
+};
+
+const createStudent = (nome) => {
+  estudante.name = nome; 
+  estudante.feedback = () => feedback(); 
+  // console.log(Object.keys(estudante));
+  // console.log(Object.values(estudante));
+  return estudante;
+};
+createStudent('Leandrão, o Lobo Solitário');
+
+module.exports = createStudent;
---
+++ src/objPlayground.js
@@ -0,0 +1,70 @@
+/*
+  Implemente a função calculator que recebe dois números inteiros como parâmetro e retorna um objeto com as seguintes chaves:
+    - sum;
+    - mult;
+    - div;
+    - sub.
+  Para cada chave atribua como valor a operação correspondente à sua chave:
+    - sum: retorna o resultado da soma dos dois números;
+    - mult: retorna o resultado da multiplicação dos dois números;
+    - div: retorna o resultado da divisão dos dois números;
+    - sub: retorna o resultado da subtração dos dois números.
+
+  Os resultados das divisões devem sempre ser arredondados para baixo.
+
+  Parâmetros:
+  - Dois números inteiros.
+
+  Comportamento:
+  calculator(1, 2); // { sum: 3, mult: 2, div: 0, sub: -1 }
+
+  Depois de ter implementado a função calculator, desenvolva uma função que converta objetos em arrays, de chaves, valores ou ambos.
+  A sua função deve receber dois parâmetros:
+
+    * o primeiro parâmetro deve ser uma string que indica o tipo de conversão;
+    * o segundo parâmetro deve ser um objeto semelhante ao que é retornado pela função calculator que você acabou de desenvolver.
+
+  Parâmetros:
+  - Uma string que indica o tipo de conversão;
+  - Um objeto no formato { sum: 3, mult: 2, div: 0, sub: -1 };
+
+  Comportamento:
+  arrayGenerator('keys', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ 'sum', 'mult', 'div', 'sub' ]
+  arrayGenerator('values', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ 3, 2, 0, -1 ]
+  arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
+*/
+
+let objectCalculator = {};
+
+const calculator = (number1, number2) => {
+  let soma = number1 + number2;
+  let multiplicacao = number1 * number2;
+  let divisao = Math.floor(number1 / number2);
+  let subtracao = number1 - number2;
+  objectCalculator.sum = soma;
+  objectCalculator.mult = multiplicacao;
+  objectCalculator.div = divisao;
+  objectCalculator.sub = subtracao;
+  console.log(objectCalculator);
+
+  return objectCalculator;
+};
+calculator(5, 2);
+
+const arrayGenerator = (type, object) => { 
+  let res;
+  if (type === 'keys') {
+    res = Object.keys(object);
+  }
+  if (type === 'values') {
+    res = Object.values(object); 
+  }
+  if (type === 'entries') {
+    res = Object.entries(object);
+  }
+  
+  return res;
+};
+arrayGenerator('keys', { sum: 4, mult: 3, div: 0, sub: -2 });
+
+module.exports = { calculator, arrayGenerator };
